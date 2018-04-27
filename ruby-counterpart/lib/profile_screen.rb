a5e99b0731b4942a07cff2d08ef69c6b6aa2847e
Tara <cheeseyx@gmail.com>
2018-04-26 21:11:06 -0500

    This should have been part of the last commit

--- ruby-counterpart/lib/profile_screen.rb
+++ ruby-counterpart/lib/profile_screen.rb
@@ -6,6 +6,9 @@ ProfileScreen = Model.new(:user, :app_theme) do
     # This line looks remarkably like Swift -- but doesn't work.
     # What is different? What needs to change?
 
+    user.avatar&.style&.background_color || app_theme.background_color
+    # In ruby, nil&.style returns nil, so nil&.style.background_color is the same as nil.background_color
+    # I'm not completely sure why this isn't an issue in Swift
 
   end
 end
